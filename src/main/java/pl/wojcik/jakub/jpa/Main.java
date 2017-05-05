96625b74b45d4f571e65706b97782abf1c4fff31
Jakub Wójcik <wujek444@gmail.com>
2017-05-05 19:09:44 +0200

    Dodawanie pól różnych typów

---
+++ src/main/java/pl/wojcik/jakub/jpa/Main.java
@@ -0,0 +1,18 @@
+package pl.wojcik.jakub.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
---
+++ src/main/java/pl/wojcik/jakub/jpa/domain/MyEnum.java
@@ -0,0 +1,5 @@
+package pl.wojcik.jakub.jpa.domain;
+
+public enum MyEnum {
+	ONE, TWO, THREE
+}
